8c31f915d6e02e2ad619b5022ecd4a5594d4633b
cdnninja <jaydenaphillips@gmail.com>
2024-04-02 20:19:00 -0600

    Update VehicleManager.py

--- audi_connect_api/VehicleManager.py
+++ audi_connect_api/VehicleManager.py
@@ -6,3 +6,9 @@ _LOGGER = logging.getLogger(__name__)
 class VehicleManager:
     def __init__(self):
         pass
+
+    def initialize(self) -> None:
+        pass
+
+    def update_all_vehicles_with_cached_state(self) -> None:
+        pass
